f9da68b1618b4db757b0b4aef6b54920ba4d7690
Karina Song <ksysong@gmail.com>
2022-11-21 10:37:56 -0800

    testing

--- public/partials/global/header.php
+++ public/partials/global/header.php
@@ -5,7 +5,7 @@
     <nav class="gn">
         <ul class="d-flex">
             <li><a href="<?php echo get_public_url('/pages/design.php'); ?>" title="Design Work">Design</a></li>
-            <li><a href="<?php echo get_public_url('/pages/dev.php'); ?>" title="Development">Development</a></li>
+            <li><a href="<?php echo get_public_url('/pages/dev.php'); ?>" title="Development">Development!!</a></li>
             <li><a href="<?php echo get_public_url('/pages/about.php'); ?>" title="About">About</a></li>
         </ul>
     </nav>
