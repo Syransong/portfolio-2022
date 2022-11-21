e491350addac63884053e97926357d0cb99b9e63
Karina Song <ksysong@gmail.com>
2022-11-21 11:55:20 -0800

    set up html skeleton

---
+++ public/projects/linmon.php
@@ -0,0 +1,74 @@
+<?php
+    require('../../app/init.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <?php require(get_project_path('public/partials/global/head.php')); ?>
+    <body>
+        <div>
+        <?php require(get_project_path('public/partials/global/header.php')); ?>
+            <main class="linmon">
+                <section class="project-bnr m-top grid">
+                    <div class="proj-bnr-info">
+                        <ul class="d-flex">
+                            <li>Packaging |</li>
+                            <li>Logo |</li>
+                            <li>Brand Identity</li>
+                        </ul>
+                        <h1></h1>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </section>
+                <section class="project-info">
+                    <ul class="d-flex">
+                        <li>
+                            <h3>Date</h3>
+                            <p>September 2022</p>
+                        </li>
+                        <li>
+                            <h3>Role</h3>
+                            <p>Graphic Designer</p>
+                        </li>
+                        <li>
+                            <h3>Software</h3>
+                            <p>Adobe Illustrator</p>
+                            <p>Adobe Dimension</p>
+                        </li>
+                        <li>
+                            <h3>Deliverables</h3>
+                            <p>Ad Poster (using Illustrator)</p>
+                            <p>Ad Poster (using Dimension)</p>
+                        </li>
+                    </ul>
+                </section>
+                <section class="project-summary">
+                    <h2>Summary</h2>
+                    <ul class="grid">
+                        <li class="col col-4">
+                            <p class="num1">01</p>
+                            <h3>Brainstorm</h3>
+                            <p></p>
+                        </li>
+                        <li class="col col-4">
+                            <p class="num2">02</p>
+                            <h3>Asset Creation</h3>
+                            <p></p>
+                        </li>
+                        <li class="col col-4">
+                            <p class="num3">03</p>
+                            <h3>Design</h3>
+                            <p></p>
+                        </li>
+                    </ul>
+                </section>
+                <section class="grid">
+                    <h2 class="col"></h2>
+                    <p class="col col-10"></p>
+                </section>
+                <a href="#" class="btn">More Projects</a>
+            </main>
+            <?php require('../partials/global/footer.php'); ?> 
+        </div>
+    </body>
+</html>
