4e61b5adbc45b2b877bd17998f36da9f28959697
Karina Song <ksysong@gmail.com>
2022-11-22 02:19:30 -0800

    add some copy to ematcha file

--- public/projects/ematcha.php
+++ public/projects/ematcha.php
@@ -25,7 +25,7 @@
                     <ul class="d-flex">
                         <li>
                             <h3>Date</h3>
-                            <p>September 2022</p>
+                            <p>May 2022</p>
                         </li>
                         <li>
                             <h3>Role</h3>
@@ -33,7 +33,9 @@
                         </li>
                         <li>
                             <h3>Software</h3>
-                            <p>Adobe Illustrator</p>
+                            <p>Adobe Photoshop</p>
+                            <p>Adobe InDesign</p>
+                            <p>Adobe After Effects</p>
                             <p>Adobe Dimension</p>
                         </li>
                         <li>
@@ -52,23 +54,42 @@
                         <li class="col col-4">
                             <p class="num1">01</p>
                             <h3>Client Brief</h3>
-                            <p></p>
+                            <p>Exchange a brand and product idea with a fellow student</p>
                         </li>
                         <li class="col col-4">
                             <p class="num2">02</p>
                             <h3>Brainstorm</h3>
-                            <p></p>
+                            <p>Brainstorm & create the company logo and colour palette</p>
                         </li>
                         <li class="col col-4">
                             <p class="num3">03</p>
                             <h3>Design</h3>
-                            <p></p>
+                            <p>Create product packaging, 3D render, poster and stationery set</p>
                         </li>
                     </ul>
                 </section>
                 <section class="grid">
-                    <h2 class="col"></h2>
-                    <p class="col col-10"></p>
+                    <h2 class="col">Client Brief </h2>
+                    <p class="col col-10">To simulate a real-life client scenario, we randomly received a fellow student's idea for a company. They would serve as our on-going client where we could receive feedback and suggestions. I received the following guidelines:</p>
+                    <ul>
+                        <li>Company Name: Emerald Matcha</li>
+                        <li>Product: Matcha Powder</li>
+                        <li>Main Colour: #00A651</li>
+                        <li>Customer Demographic: 25-45 year olds</li>
+                        <li>Competitors: Jade Leaf Matcha, Grace & Green Matcha</li>
+                    </ul>
+                </section>
+                <section>
+                    <h2>Logo Design</h2>
+                    <p>For the logo design, I wanted to draw inspiration from the company name by incorporating a matcha leaf and the shape of a gemstone. I also wanted to create a simple and clean logo to appeal to the younger customer demographic. After consulting with my client, I decided the final logo would be a matcha leaf in-laid in a tear-drop shaped gem.</p>
+                </section>
+                <section>
+                    <h2>Colour Palette</h2>
+                    <p>To go with the deep green colour given by the client, I decided to go with a Split Complementary colour scheme of green, pink and yellow. This colour scheme gives a retro trendy vibe, which encapsulates the idea of targeting a younger audience with a traditional matcha product. Pink and yellow would serve as visually-striking colours in comparison with the primary green colour.</p>
+                </section>
+                <section>
+                    <h2>Product Dimensions and Design</h2>
+                    <p></p>
                 </section>
                 <a href="#" class="btn">More Projects</a>
             </main>
