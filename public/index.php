7ed879c86f9db68163fec6ec73e1722c1595038f
Karina Song <ksysong@gmail.com>
2023-01-16 23:09:01 -0800

    wrap sections in container class

--- public/index.php
+++ public/index.php
@@ -15,14 +15,16 @@
                 require(get_project_path('public/partials/global/header.php')); 
             ?>
             <main class="page-content">
-                <section class="bnr grid m-top p-top">
-                    <div class="bnr-info col-12 col-12-sm col-12-md col-6-lg">
-                        <p>Web Development | Graphic Design</p>
-                        <h1>Karina Song</h1>
-                        <p>A human-centred designer and developer that strives to create accessible and intuitive products. Good design is when you don't have to think about it.</p>
-                        <a href="mailto:ksysong@gmail.com" title="Email me" class="btn">Contact Me</a>
+                <section class="bnr m-top p-top">
+                    <div class="container grid">
+                        <div class="bnr-info col-12 col-12-sm col-12-md col-6-lg">
+                            <p>Web Development | Graphic Design</p>
+                            <h1>Karina Song</h1>
+                            <p>A human-centred designer and developer that strives to create accessible and intuitive products. Good design is when you don't have to think about it.</p>
+                            <a href="mailto:ksysong@gmail.com" title="Email me" class="btn">Contact Me</a>
+                        </div>
+                        <img src="<?php echo get_public_url('/images/banner-img.svg'); ?>" class="col-12 col-12-sm col-12-md col-6-lg" alt="A turquoise arch with a pink circle at the top right corner and a golden star on the bottom left">
                     </div>
-                    <img src="<?php echo get_public_url('/images/banner-img.svg'); ?>" class="col-12 col-12-sm col-12-md col-6-lg" alt="A turquoise arch with a pink circle at the top right corner and a golden star on the bottom left">
                 </section>
                 <section class="skills">
                     <div class="container">
@@ -47,34 +49,36 @@
                     </div>
                 </section>
                 <section class="latest-work">
-                    <h2>Latest Work</h2>
-                    <ul class="grid">
-                        <li class="col-12 col-6-md col-4-lg">
-                            <a href="<?php echo get_public_url('/projects/linmon.php');?>">
-                                <img src="<?php echo get_public_url('/images/linmon-square-tn.jpg'); ?>" alt="A mockup up of a glass bottle and a juicebox of Linmon Lemon tea">
-                                <h3>Linmon</h3>
-                                <p>Product Design, Brand Identity</p>
-                                <a href="<?php echo get_public_url('/projects/linmon.php');?>">View Project &rarr;</a>
-                            </a>
-                        </li>
-                        <li class="col-12 col-6-md col-4-lg">
-                            <a href="<?php echo get_public_url('/projects/ematcha.php');?>">
-                                <img src="<?php echo get_public_url('/images/ematcha-square-tn.jpg'); ?>" alt="Mockup of the Emerald Matcha stationery set">
-                                <h3>Emerald Matcha</h3>
-                                <p>Product Design, Brand Identity</p>
-                                <a href="<?php echo get_public_url('/projects/ematcha.php');?>">View Project &rarr;</a>
-                            </a>
-                        </li>
-                        <li class="col-12 col-6-md col-4-lg">
-                            <a href="<?php echo get_public_url('/projects/tliving.php');?>">
-                                <img src="<?php echo get_public_url('/images/tliving-square-tn.jpg'); ?>" alt="Preview of Tiny Living's landing page">
-                                <h3>Tiny Living</h3>
-                                <p>Print Design, Web Design</p>
-                                <a href="<?php echo get_public_url('/projects/tliving.php');?>">View Project &rarr;</a>
-                            </a>
-                        </li>
+                    <div class="container">
+                        <h2>Featured Work</h2>
+                        <ul class="grid">
+                            <li class="col-12 col-6-md col-4-lg">
+                                <a href="<?php echo get_public_url('/projects/linmon.php');?>">
+                                    <img src="<?php echo get_public_url('/images/linmon-square-tn.jpg'); ?>" alt="A mockup up of a glass bottle and a juicebox of Linmon Lemon tea">
+                                    <h3>Linmon</h3>
+                                    <p>Product Design, Brand Identity</p>
+                                    <a href="<?php echo get_public_url('/projects/linmon.php');?>">View Project &rarr;</a>
+                                </a>
+                            </li>
+                            <li class="col-12 col-6-md col-4-lg">
+                                <a href="<?php echo get_public_url('/projects/ematcha.php');?>">
+                                    <img src="<?php echo get_public_url('/images/ematcha-square-tn.jpg'); ?>" alt="Mockup of the Emerald Matcha stationery set">
+                                    <h3>Emerald Matcha</h3>
+                                    <p>Product Design, Brand Identity</p>
+                                    <a href="<?php echo get_public_url('/projects/ematcha.php');?>">View Project &rarr;</a>
+                                </a>
+                            </li>
+                            <li class="col-12 col-6-md col-4-lg">
+                                <a href="<?php echo get_public_url('/projects/tliving.php');?>">
+                                    <img src="<?php echo get_public_url('/images/tliving-square-tn.jpg'); ?>" alt="Preview of Tiny Living's landing page">
+                                    <h3>Tiny Living</h3>
+                                    <p>Print Design, Web Design</p>
+                                    <a href="<?php echo get_public_url('/projects/tliving.php');?>">View Project &rarr;</a>
+                                </a>
+                            </li>
+                    </div>
                     </ul>
-                    <a href="<?php echo get_public_url('/pages/design.php');?>" class="btn">View More</a>
+                    <!-- <a href="<?php echo get_public_url('/pages/design.php');?>" class="btn">View More</a> -->
                 </section>
                 <?php require(get_project_path('public/partials/components/get-connected.php')); ?>
             </main>
