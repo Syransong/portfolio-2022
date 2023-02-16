4751f0686b53138595cbedb27efe8234280f086c
Karina Song <ksysong@gmail.com>
2023-02-16 15:25:32 -0800

    set up lazyload js plugin

--- public/partials/global/footer.php
+++ public/partials/global/footer.php
@@ -40,3 +40,7 @@
         ';
     }
 ?>
+<script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.8.3/dist/lazyload.min.js"></script>
+<?php 
+    const lazyLoadInstance = new LazyLoad({}); 
+?>
