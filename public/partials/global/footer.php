114592d7b5d7b27ed64978c86ddd776141b79e6a
Karina Song <ksysong@gmail.com>
2023-02-23 14:44:19 -0800

    change back to top button to button element

--- public/partials/global/footer.php
+++ public/partials/global/footer.php
@@ -1,5 +1,5 @@
 <footer class="d-flex" id="footer">
-    <a id="scroll-top" title="Back to Top" href="#"></a>
+    <button id="scroll-top" title="Back to Top"></button>
     <p>&copy; Karina Song 2022</p>
     <ul class="d-flex">
         <li>
