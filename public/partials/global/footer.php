fc1958c2f8069004190decc7bfd7f5d9ce1a10c9
Karina Song <ksysong@gmail.com>
2023-03-06 23:18:40 -0800

    add max width to footer

--- public/partials/global/footer.php
+++ public/partials/global/footer.php
@@ -1,33 +1,35 @@
-<footer class="d-flex" id="footer">
-    <button id="scroll-top" title="Back to Top"></button>
-    <p>&copy; Karina Song 2022</p>
-    <ul class="d-flex soc-med">
-        <li>
-            <a href="mailto:ksysong@gmail.com" title="Email me">
-                <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('/images/email-icon.svg'); ?>" alt="Email Icon">
-            </a>
-        </li>
-        <li>
-            <a href="https://github.com/Syransong" title="Go to my Github" target="_blank">
-                <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('/images/github-logo.sv'); ?>g" alt="Github Logo">
-            </a>
-        </li>
-        <li>
-            <a href="https://www.linkedin.com/in/karina-sy-song/" title="Go to my LinkedIn" target="_blank">
-                <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('/images/linkedin-logo.'); ?>svg" alt="LinkedIn Logo">
-            </a>
-        </li>
-        <li>
-            <a href="https://www.instagram.com/si1yuan2/" title="Go to my Instagram" target="_blank">
-                <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('/images/instagram-logo'); ?>.svg" alt="Instagram Logo">
-            </a>
-        </li>
-        <li>
-            <a href="https://ksysong.medium.com/" title="Go to my Medium" target="_blank">
-                <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('/images/medium-logo.sv'); ?>g" alt="Medium Logo">
-            </a>
-        </li>
-    </ul>
+<footer id="footer">
+    <div class="footer d-flex container">
+        <button id="scroll-top" title="Back to Top"></button>
+        <p>&copy; Karina Song 2022</p>
+        <ul class="d-flex soc-med">
+            <li>
+                <a href="mailto:ksysong@gmail.com" title="Email me">
+                    <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('/images/email-icon.svg'); ?>" alt="Email Icon">
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/Syransong" title="Go to my Github" target="_blank">
+                    <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('/images/github-logo.sv'); ?>g" alt="Github Logo">
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/karina-sy-song/" title="Go to my LinkedIn" target="_blank">
+                    <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('/images/linkedin-logo.'); ?>svg" alt="LinkedIn Logo">
+                </a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/si1yuan2/" title="Go to my Instagram" target="_blank">
+                    <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('/images/instagram-logo'); ?>.svg" alt="Instagram Logo">
+                </a>
+            </li>
+            <li>
+                <a href="https://ksysong.medium.com/" title="Go to my Medium" target="_blank">
+                    <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('/images/medium-logo.sv'); ?>g" alt="Medium Logo">
+                </a>
+            </li>
+        </ul>
+    </div>
 </footer>
 <script src="<?php echo get_public_url('js/scroll-top.js')?>"></script>
 <script src="<?php echo get_public_url('js/hamburger-menu.js')?>"></script>
