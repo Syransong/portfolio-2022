24e79a003d01f41e1ef143cda3c92225257a4d47
Karina Song <ksysong@gmail.com>
2023-03-21 22:09:59 -0700

    wrapped large card in a tag to make card clickable

--- public/partials/projects/large-card.php
+++ public/partials/projects/large-card.php
@@ -1,15 +1,17 @@
-<div class="large-card d-flex">
-    <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('images/') . $project->large_img_url; ?>" alt="<?php echo $project->alt ?>">
-    <div class="large-card-info">
-        <ul class="d-flex">
-            <?php 
-                foreach ($project->tags as $tag) {
-                    echo '<li>' . $tag . '</li>';
-                }
-            ?> 
-        </ul>
-        <h1><?php echo h($project->name); ?></h1>
-        <p><?php echo h($project->description); ?></p>
-        <a href="<?php echo get_public_url('projects/') . $project->project_url; ?>">View Project &rarr;</a>
+<a class="large-card-wrapper" href="<?php echo get_public_url('projects/') . $project->project_url; ?>">    
+    <div class="large-card d-flex">
+        <img class="lazy" src="<?php echo get_public_url('/images/placeholder.svg'); ?>" data-src="<?php echo get_public_url('images/') . $project->large_img_url; ?>" alt="<?php echo $project->alt ?>">
+        <div class="large-card-info">
+            <ul class="d-flex">
+                <?php 
+                    foreach ($project->tags as $tag) {
+                        echo '<li>' . $tag . '</li>';
+                    }
+                ?> 
+            </ul>
+            <h1><?php echo h($project->name); ?></h1>
+            <p><?php echo h($project->description); ?></p>
+            <a class="proj-link">View Project &rarr;</a>
+        </div>
     </div>
-</div>
+</a>
